3a17934fdfac5a21f3ab8f3dccb6ec125049462c
Jensen <theis.jensen@yale.edu>
2024-03-11 13:50:10 -0400

    slurm job file

---
+++ tc_slurm_job.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH -J tc                           # Job name
+#SBATCH -o slurm_output/output_%j.txt                # Output file (%j will be replaced by the job ID)
+#SBATCH -e slurm_output/error_%j.txt                 # Error file (%j will be replaced by the job ID)
+#SBATCH --ntasks=1                      # Number of tasks (processes)
+#SBATCH --cpus-per-task=4              # Number of CPU cores per task
+#SBATCH --mem=20G                      # Memory per node (450 GB)
+#SBATCH --partition=day
+#SBATCH --time=10:00
+
+echo '-------------------------------'
+cd ${SLURM_SUBMIT_DIR}                  # Change directory to slurm submit directory
+echo ${SLURM_SUBMIT_DIR}
+echo Running on host $(hostname)
+echo Time is $(date)
+echo SLURM_NODES are $(echo ${SLURM_NODELIST})
+echo '-------------------------------'
+echo -e '\n\n'
+
+# Load any necessary modules
+module load R/4.3.0-foss-2020b                        
+
+# Your R script execution command
+Rscript "Code/main.R"
