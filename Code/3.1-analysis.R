b7ad16b608e3de9956f847af1dcffbe1b71ebb29
Jensen <theis.jensen@yale.edu>
2024-03-12 11:30:33 -0400

    Added relationship with citations

--- Code/3.1-analysis.R
+++ Code/3.1-analysis.R
@@ -106,6 +106,33 @@ estimates |>
   )
 ggsave("figures/regs_us.pdf", width=lt_w, height=lt_h)
 
+# Expected return in the beginning versus end of the sample, in and out of the U.S. ----------------
+estimates[model=="time", .(feature, term, estimate)] |> 
+  pivot_wider(names_from = term, values_from = estimate) |>
+  mutate(
+    est_start = var,
+    est_end = var+`var:t`*as.integer(interval(set$start+1, set$end+1) / months(1)),
+  ) |> 
+  mutate(sort_order = est_end) |> 
+  select(feature, sort_order, est_start, est_end) |> 
+  pivot_longer(c(est_start, est_end)) |> 
+  mutate(
+    name = case_when(
+      name == "est_start" ~ "Expected return in 1985",
+      name == "est_end" ~ "Expected return in 2023"
+    )
+  ) |>
+  ggplot(aes(x=reorder(feature, sort_order), y = value*12, fill = name)) +
+  coord_flip() +
+  geom_col(position = "dodge") + 
+  facet_wrap(~name) +
+  labs(y = "Expected annualized return", title = "Expected returns in 1985 vs. 2023") +
+  theme(
+    axis.title.y = element_blank(),
+    legend.position = "none"
+  )
+ggsave("figures/er_start_end.pdf", width=lt_w, height=lt_h)  
+
 # Correlation with citations ----------------
 library(xtable)
 est_with_cites <- theme_info[, .("feature"=cluster, cites)][estimates, on = "feature"][!is.na(cites)] |> 
@@ -128,4 +155,27 @@ est_with_cites[, .(
   xtable::xtable(caption = "Spearman correlation of citations and estimate", label = "tbl:cor:new") |> 
   print.xtable(include.rownames = FALSE, caption.placement = "bottom", sanitize.text.function = identity) 
 
-
+# Figure with citations:
+theme_info[, .("feature"=cluster, cites)][estimates, on = "feature"][!is.na(cites)] |> 
+  filter((model == "none" & term=="var") | term %in% all_ints) |> 
+  mutate(
+    model = case_when(
+      model == "none" ~ "Raw (beta_1)",
+      model == "time" ~ "Time (beta_2)",
+      model == "liq" ~ "Size (beta_2)",
+      model == "rvol" ~ "Volatility (beta_2)",
+      model == "region" ~ "Developed (beta_2)",
+      model == "us" ~ "US (beta_2)"
+    ) |> fct_relevel("Raw (beta_1)", "Time (beta_2)", "Size (beta_2)", "Volatility (beta_2)", "Developed (beta_2)", "US (beta_2)")
+  ) |> 
+  ggplot(aes(x=reorder(feature, cites), y = estimate)) +
+  coord_flip() +
+  geom_col(fill = colours_theme[1]) +
+  geom_hline(yintercept = 0, linetype = "solid", color = "black") + 
+  geom_errorbar(aes(ymin = estimate - 1.96*std.error, ymax = estimate + 1.96*std.error), width = 0.25, position = position_dodge(0.9)) +
+  labs(title = "Factors on y-axis sorted by citations", y = "Estimate")  +
+  theme(
+    axis.title.y = element_blank()
+  ) + 
+  facet_wrap(~model, scales = "free_x")
+ggsave("figures/regs_cites.pdf", width=lt_w, height=lt_h*1.5)
